37ea7b55fdf276e9811897d9f76e0b3d5c7306f6
Carlox <cmarinbotella@gmail.com>
2023-05-12 10:47:49 +0200

    Project in SRC

--- src/DataType.java
+++ src/DataType.java
@@ -12,4 +12,9 @@ public class DataType {
             default -> "STRING";
         };
     }
+    
+    public static String gdArray(int code){
+        int codeData[] = new int[]{702, 1082, 1083, 1114, 1184, 1266, 12403};
+        int codenumeric[] = new int[]{20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396};
+    }
 }
