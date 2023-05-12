1e8ceb916d9967c7ddab8020de7234ca041b4a83
Carlox <cmarinbotella@gmail.com>
2023-05-12 10:43:21 +0200

    Hola

---
+++ src/DataType.java
@@ -0,0 +1,15 @@
+public class DataType {
+    public static void main(String[] args) {
+        System.out.println(DataType.gd(20));
+        System.out.println(DataType.gd(702));
+        System.out.println(DataType.gd(0));
+    }
+    //Refactor no Vector, no Matrix.
+    public static String gd(int code) {
+        return switch (code) {
+            case 702, 1082, 1083, 1114, 1184, 1266, 12403 -> "DATE";
+            case 20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396 -> "NUMERIC";
+            default -> "STRING";
+        };
+    }
+}
