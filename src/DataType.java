885f08da4b9b342073122ec30fa138ca28430601
Carlos Marín <72863936+TheCarlitosYT@users.noreply.github.com>
2023-05-13 10:50:56 +0200

    Add files via upload

--- src/DataType.java
+++ src/DataType.java
@@ -4,6 +4,7 @@ public class DataType {
         System.out.println(DataType.gd(702));
         System.out.println(DataType.gd(0));
     }
+
     //Refactor no Vector, no Matrix.
     public static String gd(int code) {
         return switch (code) {
@@ -12,9 +13,21 @@ public class DataType {
             default -> "STRING";
         };
     }
-    
-    public static String gdArray(int code){
+
+    public static String gdArray(int code) {
         int codeData[] = new int[]{702, 1082, 1083, 1114, 1184, 1266, 12403};
         int codenumeric[] = new int[]{20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396};
+
+        for (int n : codeData) {
+            if (n == code) {
+                return "DATA";
+            }
+        }
+        for (int n : codenumeric) {
+            if (n == code) {
+                return "NUMERIC";
+            }
+        }
+        return "STRING";
     }
 }
